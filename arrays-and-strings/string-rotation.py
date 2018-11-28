75a436cf63066bbef66652d2c352e923a5592367
anantkaushik <anant.kaushik2@gmail.com>
2018-11-29 00:14:59 +0530

    String Rotation

---
+++ arrays-and-strings/string-rotation.py
@@ -0,0 +1,10 @@
+"""
+Assume you have a method isSubstring which checks if one  word is a substring of another. Given two strings, 
+s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isDubstring(eg. "waterbottle" is a 
+rotation of "erbottlewat".)
+"""
+def isRotation(s1,s2):
+    if len(s1) != len(s2):
+        return False
+    newS = s1 + s1
+    return s2 in newS # Alternative of IsSubstring method.
